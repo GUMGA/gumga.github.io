c96415d4d992dfdd81c57709a62499086eede0f7
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-01-17 11:46:16 -0200

    no cache http provider

--- routers.js
+++ routers.js
@@ -1,6 +1,7 @@
 export default function Routers(
     $stateProvider
-    ,$urlRouterProvider){
+    ,$urlRouterProvider
+    ,$httpProvider){
 
       $urlRouterProvider.otherwise('/app/home');
 
@@ -11,8 +12,14 @@ export default function Routers(
               template: '<div ui-view></div>'
           })
 
+
+      $httpProvider.defaults.headers.common['Pragma'] = 'no-cache';
+      $httpProvider.defaults.headers.common["Cache-Control"] = "no-cache";
+      $httpProvider.defaults.headers.common["If-Modified-Since"] = "0";
+
 }
 
 Routers.$inject = [
     '$stateProvider'
-    ,'$urlRouterProvider'];
+    ,'$urlRouterProvider'
+    ,'$httpProvider'];
