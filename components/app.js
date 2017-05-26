0714ba905cace7ad3d5e5208faedd6191fd8b972
Gui Seek <guiseek@gmail.com>
2017-05-26 14:15:43 -0300

    Update app.js

--- components/app.js
+++ components/app.js
@@ -4,6 +4,7 @@ angular.module('app')
     var ctrl = this;
     var url = location.href.split('/')
     ctrl.version = url[url.length - 1] ? url[url.length - 1] : url[url.length - 2]
+    window.version = ctrl.version
 
     $http.get('../versions.json').then(function(resp){
       ctrl.versions = resp.data;
