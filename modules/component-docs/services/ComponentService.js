9694b2be7e355a1bf7354079b23e824d09d5a1c3
Gumga Tecnologia da Informação S/A <gumgait@gmail.com>
2017-06-01 17:02:03 -0300

    Update ComponentService.js

--- modules/component-docs/services/ComponentService.js
+++ modules/component-docs/services/ComponentService.js
@@ -1,6 +1,6 @@
 export default function ComponentService($http){
     function getRepos() {
-        return $http.get('https://api.github.com/users/GUMGA/repos?access_token=4b648a7c4221786b206d72befda884bbb8531556')
+        return $http.get('https://api.github.com/users/GUMGA/repos?access_token=66d50ddaf48b775369aa6484610dd04b594e205c')
     }
 
     const Service = {
